7c54a1bd52742f65e6cdf7d29cdf5a1db3920131
muhammad fajar alanis <fajaralanis27@gmail.com>
2026-09-22 00:47:35 +0700

    Add files via upload

---
+++ products.php
@@ -0,0 +1,9 @@
+<?php
+$products = [
+ ["id"=>"K001","nama"=>"Kopi Gayo Arabika","kategori"=>"Arabika","harga"=>85000,"stok"=>10,"deskripsi"=>"Kopi Arabika Gayo dengan aroma khas dan cita rasa lembut"],
+ ["id"=>"K002","nama"=>"Kopi Robusta Aceh","kategori"=>"Robusta","harga"=>65000,"stok"=>2,"deskripsi"=>"Kopi Robusta Aceh dengan rasa kuat dan aroma khas"],
+ ["id"=>"K003","nama"=>"Kopi Arabika Toraja","kategori"=>"Arabika","harga"=>90000,"stok"=>7,"deskripsi"=>"Kopi Arabika Toraja dengan rasa seimbang dan aroma harum"],
+ ["id"=>"K004","nama"=>"Kopi Liberika","kategori"=>"Liberika","harga"=>70000,"stok"=>1,"deskripsi"=>"Kopi Liberika dengan karakter rasa unik dan aroma kuat"],
+ ["id"=>"K005","nama"=>"Kopi Susu Gula Aren","kategori"=>"Minuman Kopi","harga"=>25000,"stok"=>15,"deskripsi"=>"Minuman kopi susu dengan campuran gula aren"]
+];
+?>
